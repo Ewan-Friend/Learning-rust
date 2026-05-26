b47f868a0ec713541e482499e6ccd20e5cf044fa
ewan <ewan.friend@hotmail.com>
2026-05-27 00:51:29 +0100

    some ownership stuff

---
+++ ownership/src/main.rs
@@ -0,0 +1,18 @@
+/* ----- Pushing on a heap -----
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(", world!");
+
+    println!("{s}");
+}
+*/
+
+/* ----- CLONING STRING -----
+*/
+fn main() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone();    
+
+    println!("s1 = {s1}, s2 = {s2}");
+}
