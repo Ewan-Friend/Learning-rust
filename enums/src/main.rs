3bbbe47bac039f8dd4960e0ec7277c119cb3fe93
ewan <ewan.friend@hotmail.com>
2026-06-04 19:28:47 +0100

    control flow discovered

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,7 +1,3 @@
-/* 
- * ----- Coin Matching ----- 
- *
-
 #[derive(Debug)]
 enum UsState{
     Alabama,
@@ -12,12 +8,26 @@ enum UsState{
     // The rest...
 }
 
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+       match self {
+           UsState::Alabama => year >= 1819,
+           UsState::Alaska => year >= 1959,
+           _ => {println!("Not a registered state!"); false}
+       } 
+    }
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
     Quarter(UsState) 
 }
+
+/* 
+ * ----- Coin Matching ----- 
+ *
 
 fn value_in_cents(coin: Coin) -> u8{
     match coin {
@@ -61,7 +71,6 @@ fn main(){
 
 /*
  * ----- Catch all & '_' -----
- */
 fn main(){
     let dice_roll = 9;
     match dice_roll {
@@ -73,4 +82,45 @@ fn main(){
     fn do_this_cool_thing() {}
     fn undo_cool_thing_and_do_bad_thing() {}
     fn reroll() {}
+}
+ */
+
+/*
+ * ----- If Let Control Flow ----- 
+ *
+ *fn main(){
+ *    let config_max = Some(3u8);
+ *    match config_max {
+ *      Some(max) => println!("maximum is configured to be {max}"),
+ *       _ => (),
+ *   }
+ *}
+ * Is equal to:
+ *
+ fn main(){
+     let config_max = Some(3u8);
+     if let Some(max) = config_max {
+         println!("The maximum is configured to be {max}");
+     }
+ }
+ */
+
+/*
+ * ----- Let Else Control Flow ----- 
+ */
+
+fn main(){
+    describe_state_quarter(Coin::Quarter(UsState::Alabama));
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+   let Coin::Quarter(state) = coin else {
+       return None;
+   };
+
+   if state.existed_in(1900){
+       Some(format!("{state:?} is pretty old, for America!"))
+   } else {
+       Some(format!("{state:?} is relatively new."))
+   }
 }
