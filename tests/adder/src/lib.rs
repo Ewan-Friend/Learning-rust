e091c68525e6f4161c60a4db00aadf134f5c3aaa
ewan <ewan.friend@hotmail.com>
2026-06-21 22:31:18 +0100

    finished learning tests

--- tests/adder/src/lib.rs
+++ tests/adder/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: u64) -> u64 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(left: u64, right: u64) -> u64 {
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,8 +22,26 @@ mod tests {
     }
 
     // ----- Failing test -----
+//    #[test]
+//    fn it_fails() {
+//        panic!("Make this test fail");
+//    }
+    // ----- Using Result Syntax ----- 
     #[test]
-    fn it_fails() {
-        panic!("Make this test fail");
+    fn _it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4{
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }   
+
+    // ----- Can test private functions -----
+    #[test]
+    fn internal() {
+        let result = internal_adder(2, 2);
+        assert_eq!(result, 4);
     }
 }
