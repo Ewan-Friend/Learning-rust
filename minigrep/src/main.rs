e89b0cee6c4e21ff8b3654bab4be977ea26061af
ewan <ewan.friend@hotmail.com>
2026-06-22 14:57:49 +0100

    completed minigrep with environment variables and seperate error printing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,21 +3,18 @@ use std::fs;
 use std::process;
 use std::error::Error;
 
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // Collect arguments
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
@@ -26,11 +23,15 @@ fn main() {
  * Reads from the file!
  */
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+    let contents = fs::read_to_string(config.file_path)?
 
-    println!("\n-----With text-----\n\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
 
@@ -38,11 +39,10 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool, 
 }
-
-
 
 impl Config {
     /**
@@ -56,6 +56,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { 
+            query, 
+            file_path, 
+            ignore_case,
+        })
     }
 }
