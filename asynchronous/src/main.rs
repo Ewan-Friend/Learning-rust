f156b66d82f9712d72e885d080ca192a1a3c63bd
ewan <ewan.friend@hotmail.com>
2026-07-04 14:31:55 +0100

    a bunch of asynchronous stuff

--- asynchronous/src/main.rs
+++ asynchronous/src/main.rs
@@ -1,4 +1,6 @@
-use trpl::{Either, Html};
+//use trpl::{Either, Html};
+/*
+ * ----- Title from URL race using asynchronous programming ------
 
 /*
  * same as:
@@ -30,6 +32,218 @@ fn main() {
         match maybe_title {
             Some(title) => println!("Its page title was: '{title}'"),
             None => println!("it had no specified title"),
-        }    
+        }
+    })
+}
+*/
+
+/*
+ * ----- Creating new task with spawn_task ---- 
+use trpl;
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let handle = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        handle.await.unwrap();
+    });
+}
+ */
+
+/*
+ * ----- Letting both futures finish using join -----
+use trpl;
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let fut1 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
+    });
+}
+ */
+
+/*
+ * ----- Message passing in async programs -----
+use trpl;
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move { // Moving drops ownership 
+                                  // Means reciever can take None after
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await{
+                println!("received '{value}'");
+            }
+        };
+        
+        let tx_fut = async move { 
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join!(tx1_fut, tx_fut, rx_fut);
+    });
+}
+ */
+
+/*
+ * ----- Yielding control to runtime for slow async functions -----
+use trpl;
+use std::{thread, time::Duration};
+
+fn main() {
+    trpl::block_on(async {
+        let a = async {
+            println!("a started.");
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished.");
+        };
+
+        let b = async {
+            println!("b started.");
+            slow("b", 30);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 20);
+            trpl::yield_now().await;
+            println!("'b' finished.");
+        };
+
+        trpl::select(a, b).await;
+    })
+}
+
+fn slow(name: &str, ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name}' ran for {ms}ms");
+}
+ */
+
+/*
+ * ----- Custom timeout async abstraction -----
+use trpl::Either;
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let slow = async {
+            trpl::sleep(Duration::from_secs(5)).await;
+            "Finally finished"
+        };
+
+        match timeout(slow, Duration::from_secs(2)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+    });
+}
+
+async fn timeout<F: Future> (
+    future: F,
+    duration: Duration,
+    ) -> Result<F::Output, Duration> {
+        match trpl::select(future, trpl::sleep(duration)).await {
+            Either::Left(output) => Ok(output),
+            Either::Right(_) => Err(duration),
+        }
+}
+
+ */
+
+/*
+ * ----- Futures in sequence: Streams -----
+use trpl::StreamExt;
+
+fn main() {
+    trpl::block_on(async {
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let iter = values.iter().map(|n| n*2);
+        let mut stream = trpl::stream_from_iter(iter);
+
+        while let Some(value) = stream.next().await {
+            println!("The value was: {value}");
+        }
+    });
+}
+ */
+
+/*
+ * ----- Using threads AND concurrency -----
+*/ 
+use std::{thread, time::Duration};
+
+fn main() {
+    let (tx, mut rx) = trpl::channel();
+
+    thread::spawn(move || {
+        for i in 1..11 {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    trpl::block_on(async {
+        while let Some(message) = rx.recv().await {
+            println!("{message}");
+        }
     })
 }
